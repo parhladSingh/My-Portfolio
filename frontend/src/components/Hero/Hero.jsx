bc78a7c0728ab1d6d424224292e91aff82d2d3af
parhladSingh <parhladsingh760@gmail.com>
2024-06-03 16:56:12 +0530

    To correct the text

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
 
         <section className={styles.container}>
             <div className={styles.content}>
-                <h1 className={styles.title}>Hello,i'm Parhlad Singh</h1>
+                <h1 className={styles.title}>Hello,I'm Parhlad Singh</h1>
                 <p className={styles.description}>🎓A pre-final year Computer Science Engineering student at BRCM College of Engineering and Technology, Bahal. And an enthusiastic tech lover on a mission to dive deeper into Full Stack Development.💻</p>
                 <a className={styles.contactBtn} href="mailto:parhladsingh760@gmail.com">Contact Me</a>
             </div>
